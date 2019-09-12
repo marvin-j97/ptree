09d002f54e5fc5322806a3a59d20ff8f1f4feb37
Alexander Janke <33967771+alexanderjanke@users.noreply.github.com>
2019-09-12 18:15:16 +0200

    more tests

--- test/unit.js
+++ test/unit.js
@@ -4,18 +4,31 @@ var should = require('chai').should();
 var ptree = require("../dist/").default;
 
 describe('Validate', () => {
-  it('should return true', () => {
-    let obj = {
-      a: 2,
-      b: "string",
-      c: {
-        d: [1, 2, 3, 4],
-        e: {
-          f: 2
-        }
+  const obj1 = {
+    a: 2,
+    b: "string",
+    c: {
+      d: [1, 2, 3, 4],
+      e: {
+        f: 2
       }
     }
-    const result = new ptree(obj).validate([{
+  }
+
+
+  it('should pass checking if the keys a, b and c exist', () => {
+    const result = new ptree(obj1).validate([{
+      key: "a",
+    }, {
+      key: "b",
+    }, {
+      key: "c"
+    }]);
+    expect(result).to.be.true;
+  });
+
+  it('should pass checking if the key a and b exist and f.d.e to be optional', () => {
+    const result = new ptree(obj1).validate([{
       key: "a",
     }, {
       key: "b",
@@ -27,5 +40,68 @@ describe('Validate', () => {
       ]
     }]);
     expect(result).to.be.true;
-  })
-})
+  });
+
+  it('should pass checking if a equals 2 and b is a string with a length greater than 0', () => {
+    const result = new ptree(obj1).validate([{
+      key: "a",
+      rules: [
+        v => v == 2
+      ]
+    }, {
+      key: "b",
+      rules: [
+        v => typeof v === "string",
+        v => v.length > 0
+      ]
+    }]);
+    expect(result).to.be.true;
+  });
+
+  it('should fail checking if a equals 2 and b is a string with a length greater than 10', () => {
+    const result = new ptree(obj1).validate([{
+      key: "a",
+      rules: [
+        v => v == 2
+      ]
+    }, {
+      key: "b",
+      rules: [
+        v => typeof v === "string",
+        v => v.length > 10
+      ]
+    }]);
+    expect(result).to.be.false;
+  });
+
+  it('should pass checking if a equals 2 and c.d has a length of 4', () => {
+    const result = new ptree(obj1).validate([{
+      key: "a",
+      rules: [
+        v => v == 2
+      ]
+    }, {
+      key: "c.d",
+      rules: [
+        v => v.length == 4
+      ]
+    }]);
+    expect(result).to.be.true;
+  });
+
+  it('should fail checking that c.e.f is greater than 10 and c.d has a length of 4', () => {
+    const result = new ptree(obj1).validate([{
+      key: "c.e.f",
+      rules: [
+        v => v > 10
+      ]
+    }, {
+      key: "c.d",
+      rules: [
+        v => v.length == 4
+      ]
+    }]);
+    expect(result).to.be.false;
+  });
+
+});
